9a6aadb2ece3080c9a0becb5356c8a23601b4864
kishanpeddaboina <peddaboinakishan@gmail.com>
2018-11-17 11:34:31 +0530

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import smtplib
-server = smtplib.SMTP('smtp.gmail.com', 587)
+server = smtplib.SMTP('smtp.gmail.com', 25)
 server.login("kpeddaboina@gmail.com", "Pittala_336")
 msg = "Hello!" 
 server.sendmail("kishanpeddaboina@gmail.com", "peddaboinakishan@gmail.com", msg)
