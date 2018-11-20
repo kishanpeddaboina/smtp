c4505c7a24161b3f2c82b35a29c549285b3c70c8
kishanpeddaboina <peddaboinakishan@gmail.com>
2018-11-20 07:28:39 +0530

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -2,7 +2,7 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("kpeddaboinam@gmail.com", "Oxalic_336")
+server.login("kpeddaboinam@gmail.com", "*******")
 server.ehlo()
 server.ehlo
 msg = "Hello!" 
